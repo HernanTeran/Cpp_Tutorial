d6f16335853c18c0f2c375fa7c5bbaff8af52bfb
Hernan Teran <70241590+hernant23@users.noreply.github.com>
2020-12-26 14:42:18 -0600

    Update Main.cpp

--- Lesson_01/Main.cpp
+++ Lesson_01/Main.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
 #include <string>
-
-/*
- * A type defines a set of possible values and a set of operations (for an object).
- * An object is some memory that holds a value of some type.
- * A value is a set of bits interpreted according to a type.
- * A variable is a named object.
- *
- * make a date and password(one to create a password and one as login) class
- */
-#include "AccountInfo.h"
+#include "Account.h"
 
 std::string getEmail();
 std::string getPassword();
-std::string getDOB();
+int getDay();
+int getMonth();
+int getYear();
 
 int main()
 {
-    std::string email, password, DOB;
+    std::string email, password;
 
     email = getEmail();
-
     password = getPassword();
 
-    DOB = getDOB();
+    int day, month, year;
 
+    std::cout << "Enter your date of birth\n";
 
+    day = getDay();
+    month = getMonth();
+    year = getYear();
+
+    Account account(email, password, day, month, year);
+
+    if (account.isAccountCreated())
+    {
+        std::cout << "Welcome to RuneScape!\n";
+    }
 
     return 0;
 }
@@ -33,7 +36,55 @@ int main()
 std::string getEmail()
 {
     std::cout << "Enter your email: ";
+
     std::string email;
     getline(std::cin, email);
+
+    std::cout << "\n\n";
+
     return email;
+}
+
+std::string getPassword()
+{
+    std::cout
+    << "Create a password\n"
+    << "Password must have at least 1 uppercase letter.\n"
+    << "Password must have at least 1 digit.\n"
+    << "Password must not contain any special characters.\n"
+    << "> ";
+
+    std::string password;
+    getline(std::cin, password);
+
+    std::cout << "\n\n";
+
+    return password;
+}
+
+int getDay()
+{
+    int day;
+
+    std::cin >> day;
+
+    return day;
+}
+
+int getMonth()
+{
+    int month;
+
+    std::cin >> month;
+
+    return month;
+}
+
+int getYear()
+{
+    int year;
+
+    std::cin >> year;
+
+    return year;
 }
