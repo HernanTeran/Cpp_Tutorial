6f32e7e797b51820f3b5c30014d60f25d3ec8772
Hernan Teran <70241590+hernant23@users.noreply.github.com>
2021-01-10 17:26:48 -0600

    Create Main.cpp

---
+++ Lesson_01/01_basics/Main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "CreamCheese.h"
+#include "Sugar.h"
+
+/*
+*			++++++++       +          +
+*			+              +          +
+*			+          +++++++++  +++++++++
+*			+              +          +
+*			++++++++       +          +
+*/
+
+template <class T>
+T getInput(T variable)
+{
+	std::cin >> variable;
+	return variable;
+}
+
+int main()
+{
+	
+
+	return 0;
+}
