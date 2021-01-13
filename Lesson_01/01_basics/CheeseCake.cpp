afff2a689b2d17076100827759eef5c1a84f056e
Hernan Teran <70241590+hernant23@users.noreply.github.com>
2021-01-12 18:02:32 -0600

    Update CheeseCake.cpp

--- Lesson_01/01_basics/CheeseCake.cpp
+++ Lesson_01/01_basics/CheeseCake.cpp
@@ -1,4 +1,25 @@
 #include "CheeseCake.h"
+using std::string;
+using std::cout;
 
 // source code file ( .cpp )
 // class definitions
+
+CheeseCake::CheeseCake(int c_cheese,
+					   double w_sugar,
+					   double s_cream,
+					   double v_extract,
+					   double salt_,
+					   int n_eggs,
+					   string crust)
+					   :
+					   cream_cheese(c_cheese),
+					   white_sugar(w_sugar),
+					   sour_cream(s_cream),
+					   vanilla_extract(v_extract),
+					   salt(salt_),
+					   n_of_eggs(n_eggs),
+					   crust_type(crust)
+{
+	cout << "Object has been created.\n";
+}
