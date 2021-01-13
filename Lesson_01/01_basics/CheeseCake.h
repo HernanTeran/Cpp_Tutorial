7b174653d93df30fb2441fb254ef45f87cd11dcf
Hernan Teran <70241590+hernant23@users.noreply.github.com>
2021-01-12 18:01:45 -0600

    Update CheeseCake.h

--- Lesson_01/01_basics/CheeseCake.h
+++ Lesson_01/01_basics/CheeseCake.h
@@ -16,13 +16,21 @@ private: // implementation
 	double sour_cream{ 0 };
 	double vanilla_extract{ 0 };
 	double salt{ 0 };
-	int eggs{ 0 };
+	int n_of_eggs{ 0 };
 	std::string crust_type;
 	
 public: // interface
 
 	// constructor
 	CheeseCake() = delete;
+	
+	CheeseCake(int c_cheese, 
+		       double w_sugar, 
+		       double s_cream, 
+		       double v_extract, 
+		       double salt_, 
+		       int n_of_eggs, 
+		       string crust);
 
 	// destructor
 	~CheeseCake() { std::cout << "Object has been destroyed by destructor.\n"; }
