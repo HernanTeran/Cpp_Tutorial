61f2692703fcc56449d30b9265784bc2635535e7
Hernan Teran <70241590+hernant23@users.noreply.github.com>
2021-01-15 14:00:49 -0600

    Update Lesson_01.cpp

--- Lesson_01/Lesson_01.cpp
+++ Lesson_01/Lesson_01.cpp
@@ -4,14 +4,18 @@
 
 
 /*
-* ideas
-* 
-* 1. gas station
-* 2. baking something
-* 3. human
-* 4. runescape account
-* 5. basketball
-* 6. glucose meter - glucometer
+* 1 - introduce parts of a C++ program/cheesecake example/cout prompts/cerr errors/introduce void function/compile time errors
+* 2 - introduce types/operators/physics/cmath library - (only into to operators)/<iomanip>/pass parameters into void/link-time errors
+* 3 - functions/operators/const-constexpr/if-else/switch/ternary/run-time errors/goal is to do functions for conversions-computation
+* 4 - loops-for-while/vectors/iterator/logic errors/exceptions/goal is to fill results(temperature) in a vector
+* 5 - functions in depth (pass by reference)/scope/namespaces/enum class/templates/do uncertainty experiment
+* 6 - structs/map/algorithms/more on vectors/iterators/lambdas/the walking physics idea
+* 7 - reading/writing files/regex/my car experiment
+* 8 - classes/RAII/copy/move/this pointer - use cheesecake as example/gas station 
+* 9 - abstract class/virtural functions/overriding/inheritance/runescape
+* 10 - sets/linked lists/arrays/matrix/example can be a song queue
+* 11 - pointers/smart pointers/example can be a bankaccount. (your money is in bankaccount, accessing through app or atm points to your money
+*      virtually.
 */
 
 /*
