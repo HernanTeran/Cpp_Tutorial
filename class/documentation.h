ef293b1fa4e30d2b290eebc6f0a14c9634660d82
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-26 07:31:13 -0600

    Create documentation.h

---
+++ class/documentation.h
@@ -0,0 +1,27 @@
+// $Header$
+//----------------------------------------------------------------------------------------------------------------------------------
+//                                               ClassName
+//----------------------------------------------------------------------------------------------------------------------------------
+// ACRONYM: Project Name
+//
+// Author: Hernan Teran
+// Created: yyyy/mm/dd
+//
+/*
+* Class
+* description
+*/
+//----------------------------------------------------------------------------------------------------------------------------------
+#ifndef SOME_NAME_H
+#define SOME_NAME_H
+#pragma once
+
+// header (.h) files first
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+// class: public, protected, private
+
+#endif // Some_Name_H   <--- this comment
